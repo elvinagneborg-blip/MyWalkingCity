79847fe891f283ec03d706787a26ff96770180f2
sandrasilfversward7 <sandrasilfversward7@gmail.com>
2026-05-20 09:34:34 +0200

    ändra tbx språk

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -128,7 +128,6 @@ watch(() => props.showReportId, (newId) => {
 })
 
 
-
 onMounted(() => {
   map = L.map(mapContainer.value).setView([59.8586, 17.6389], 13) /* skapar kartan och sätter startpositionen till centrala Uppsala, tar bort zoomkontrollerna */
   
